405eb8e5f77ec239c9468c07cbfe3b8db856e1dc
Sacha Dubois <sadubois@redhat.com>
2015-09-29 20:33:24 +0200

    changed config

---
+++ jboss-eap/6.3.0/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#docker run -itd --name redis-slave-02 redis-slave:3.0.2 /usr/bin/redis-server /etc/redis.conf
+docker run -itd --name jboss-eap jboss/jboss-eap:6.3.0 /bin/bash
+
--- redis/redis-cluster-start.sh
+++ redis/redis-cluster-start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 docker_stop () {
-  REDIS_SERVER=`docker inspect --format='{{.NetworkSettings.IPAddress}}' $1`
+  REDIS_SERVER=`docker inspect --format='{{.NetworkSettings.IPAddress}}' $1 2>/dev/null`
 
   if [ "$REDIS_SERVER" != "" ]; then
     echo "# Stoping/Deleting $1"
     docker stop $1 >/dev/null 2>&1
+    docker rm $1 >/dev/null 2>&1
+  fi
+
+  docker images $1 > /dev/null 2>&1; ret=$?
+  if [ "$ret" -eq 0 ]; then
     docker rm $1 >/dev/null 2>&1
   fi
 }
@@ -17,8 +22,8 @@ docker_stop redis-slave-02
 docker run -itd --name redis-master redis-master:3.0.2 /usr/bin/redis-server >/dev/null 2>&1
 REDIS_SERVER=`docker inspect --format='{{.NetworkSettings.IPAddress}}' redis-master`
 
-#redis-cli -h $REDIS_SERVER ping
-#redis-benchmark -h $REDIS_SERVER -q -n 1000 -c 10 -P 5
+#/usr/bin/redis-cli/redis-cli -h $REDIS_SERVER ping
+#/usr/bin/redis-cli/redis-benchmark -h $REDIS_SERVER -q -n 1000 -c 10 -P 5
 
 echo "# Build Redis Slave "
 cat redis-slave/Dockerfile.template | sed "s/REDISMASTER/$REDIS_SERVER/g" > redis-slave/Dockerfile
@@ -34,8 +39,9 @@ echo "# REDIS MASTER ....: $REDIS_SERVER"
 echo "# REDIS SLAVE-01 ..: $REDIS_SLAVE_01"
 echo "# REDIS SLAVE-02 ..: $REDIS_SLAVE_02"
 
-"redis-cli -h $REDIS_SERVER ping"
-
+echo "/usr/bin/redis-cli -h $REDIS_SERVER set hello world"
+echo "/usr/bin/redis-cli -h $REDIS_SLAVE_01 get hello"
+echo "/usr/bin/redis-cli -h $REDIS_SLAVE_02 get hello"
 
 
 
